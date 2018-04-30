2c2f0db96875d5a4f0e93acd4330bad63a9fc2fb
Vishal Arora <arorav@gmail.com>
2018-04-29 21:18:07 -0400

    Chapter 84

--- imports/api/LinksDB.js
+++ imports/api/LinksDB.js
@@ -14,7 +14,7 @@ if(Meteor.isServer){
 
 Meteor.methods({
 
-  'links.insert'(url){
+  'links.insert'(url,visible=true){
 
     const userId = Meteor.userId();
 
@@ -27,14 +27,42 @@ Meteor.methods({
         type: String,
         regEx: SimpleSchema.RegEx.Url,
         label: 'link'
+      },
+      visible: {
+        type: Boolean,
+        optional: true
       }
     }).validate({url});//end of validate
 
     LinksDB.insert({
       _id: shortid.generate(),
       url,
-      userId
+      userId,
+      visible
     });
   },//end of links.insert
+
+  'links.setVisibility'(_id,visible){
+    
+    const userId = Meteor.userId();
+
+    if(!userId){
+      throw new Meteor.Error(401,'User Not Logged In');
+    }
+    
+    new SimpleSchema({
+      _id: {
+        type  : String,
+        min   : 1,
+        label : 'short url'
+      },
+      visible: {
+        type: Boolean,
+      }
+    }).validate({_id,visible});//end of validate
+
+    LinksDB.update({_id, userId},{$set : {visible}});//end of update
+
+  },//end of links.setVisibiity
 
 });//end of methods
